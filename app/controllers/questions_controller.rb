5a5631f5730b7025241d89cd7489204a3883bae2
mathias knudsen <mathias.knudsen31@gmail.com>
2019-02-10 14:09:46 +0200

    pastouche

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @coach = "- your coach"
+    @you = "- you"
+    @question = params[:question]
+    @answer = ''
+    if @question.split[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @answer = ' Great !'
+    elsif @question == 'hello there'
+      @answer = 'GENERAL KENOBI !'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
